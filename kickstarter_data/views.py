816f4b64c9befd470676c1bf678ec5453c50d9ce
EvyHaan <evy.haan@gmail.com>
2019-04-02 13:46:03 -0700

    pagination station takeoff

--- kickstarter_data/views.py
+++ kickstarter_data/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, get_list_or_404
+from django.core.paginator import Paginator
 from .models import Project
 
 
 # Create your views here.
 
 def kickstarter_list_view(request):
-    projects = get_list_or_404(Project, id__lte = 10)
+    projects_list = get_list_or_404(Project)
+    paginator =  Paginator(projects_list, 20)
+
+    page = request.GET.get('page')
+    projects = paginator.get_page(page)
 
     context = {
         'projects': projects,
